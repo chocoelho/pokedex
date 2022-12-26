dc51db88cd5fb5710190fb1c67cd686295a737e1
Carlos Coelho <carlospecter@gmail.com>
2022-12-26 18:43:31 -0300

    Change app title

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pokédex',
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Pokédex'),
     );
   }
 }
