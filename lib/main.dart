1e52a7027c01f2eb658129f4d80763696b7c3b27
Carlos Coelho <carlospecter@gmail.com>
2022-12-26 19:38:50 -0300

    Clean up app definition
    Having a bare bones app to start with will be easier to learn things

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,20 @@
 import 'package:flutter/material.dart';
+import 'constants.dart' as constants;
 
 void main() {
-  runApp(const MyApp());
+  runApp(const PokedexApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PokedexApp extends StatelessWidget {
+  const PokedexApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pokédex',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: const PokedexHome(title: 'Pokédex'),
-    );
-  }
-}
-
-class PokedexHome extends StatefulWidget {
-  const PokedexHome({super.key, required this.title});
-  final String title;
-
-  @override
-  State<PokedexHome> createState() => _PokedexHomeState();
-}
-
-class _PokedexHomeState extends State<PokedexHome> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
+      title: constants.appName,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(constants.appName)),
+        body: const Center(child: Text('')),
       ),
     );
   }
