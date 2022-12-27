b1756fb460c0969bb794dac8f2feea4f233880f0
Carlos Coelho <carlospecter@gmail.com>
2022-12-26 23:09:18 -0300

    Define services for Generation related functions

---
+++ lib/services/generations.dart
@@ -0,0 +1,12 @@
+import 'package:pokeapi/model/game/generation.dart';
+import 'package:pokeapi/pokeapi.dart';
+import 'package:pokedex/serializers/generations.dart';
+
+Future<List<dynamic>> fetchGenerationsList() async {
+  return await PokeAPI.getObjectList<Generation>(1, 9);
+}
+
+Future<PokedexGeneration> fetchGeneration() async {
+  return PokeAPI.getObject<Generation>(1)
+      .then((generation) => PokedexGeneration(id: generation?.id, name: generation?.name));
+}
