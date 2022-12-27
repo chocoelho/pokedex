dde396c9a884832271ec113db74783d9eb51da02
Carlos Coelho <carlospecter@gmail.com>
2022-12-26 22:34:59 -0300

    Update home.dart to add multiple routes

--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pokedex/routes/games.dart';
 import 'package:pokedex/routes/generations.dart';
 import 'package:pokedex/constants.dart' as constants;
 
@@ -10,14 +11,34 @@ class PokedexHome extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text(constants.homeRouteName)),
       body: Center(
-        child: ElevatedButton(
-          child: const Text('Generations'),
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const PokedexGenerations()),
-            );
-          },
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            Center(
+              child: ElevatedButton(
+                child: const Text(constants.generationsRouteName),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const PokedexGenerations()),
+                  );
+                },
+              ),
+            ),
+            Center(
+              child: ElevatedButton(
+                child: const Text(constants.gamesRouteName),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const PokedexGames()),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
