51fa6e22f6c74dc7a6d6ba43c2c7e9d5a6d46a0a
Carlos Coelho <carlospecter@gmail.com>
2022-12-26 21:14:35 -0300

    Use routes instead of directly defining widget in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,6 @@
 import 'package:flutter/material.dart';
-import 'constants.dart' as constants;
+import 'package:pokedex/routes/home.dart';
 
 void main() {
-  runApp(const PokedexApp());
-}
-
-class PokedexApp extends StatelessWidget {
-  const PokedexApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: constants.appName,
-      home: Scaffold(
-        appBar: AppBar(title: const Text(constants.appName)),
-        body: const Center(child: Text('')),
-      ),
-    );
-  }
+  runApp(const PokedexHome());
 }
