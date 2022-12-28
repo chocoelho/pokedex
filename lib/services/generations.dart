e5eef738815f3b05832c3d90280219aed90a7f90
Carlos Coelho <carlospecter@gmail.com>
2022-12-27 21:38:09 -0300

    Ensure fetch of generations returns a list of generations

--- lib/services/generations.dart
+++ lib/services/generations.dart
@@ -2,8 +2,10 @@ import 'package:pokeapi/model/game/generation.dart';
 import 'package:pokeapi/pokeapi.dart';
 import 'package:pokedex/models/generations.dart';
 
-Future<List<dynamic>> fetchGenerationsList() async {
-  return await PokeAPI.getObjectList<Generation>(1, 9);
+Future<List<PokedexGeneration>> fetchGenerationsList() async {
+  return PokeAPI.getObjectList<Generation>(1, 9).then((generations) => generations
+      .map((generation) => PokedexGeneration(id: generation?.id, name: generation?.name))
+      .toList());
 }
 
 Future<PokedexGeneration> fetchGeneration() async {
