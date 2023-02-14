52e9be9d077801539ab7fb30fd4b45604eb82f98
Carlos Coelho <carlospecter@gmail.com>
2023-02-13 23:44:38 -0300

    Successfully list generations

--- lib/routes/generations.dart
+++ lib/routes/generations.dart
@@ -13,38 +13,33 @@ class Generations extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text(constants.generationsRouteName)),
       body: Center(
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal,
-          child: Column(
-            children: <Widget>[
-              Expanded(
-                flex: 8,
-                child: FutureBuilder<List<Generation>>(
-                  future: pokedexGenerations,
-                  builder: (context, snapshot) {
-                    if (snapshot.hasError) return Text('${snapshot.error}');
+        child: Column(
+          children: <Widget>[
+            FutureBuilder<List<Generation>>(
+              future: pokedexGenerations,
+              builder: (context, snapshot) {
+                if (snapshot.hasError) return Text('${snapshot.error}');
 
-                    return snapshot.hasData
-                        ? ListView.builder(
-                            itemCount: snapshot.data?.length,
-                            itemBuilder: (context, index) => Card(
-                              child: Text('${snapshot.data?[index].name}'),
-                            ),
-                          )
-                        : const CircularProgressIndicator();
-                  },
-                ),
+                return snapshot.hasData
+                    ? ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: snapshot.data?.length,
+                        itemBuilder: (context, index) => Card(
+                          child: Text('${snapshot.data?[index].name}'),
+                        ),
+                      )
+                    : const Center(child: CircularProgressIndicator());
+              },
+            ),
+            Flexible(
+              child: ElevatedButton(
+                child: const Text('Go back Home'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
               ),
-              Expanded(
-                child: ElevatedButton(
-                  child: const Text('Go back Home'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
