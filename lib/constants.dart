a454a879fc4e5ff92c06ae2b9d28b12976583408
Carlos Coelho <carlospecter@gmail.com>
2023-02-13 23:49:41 -0300

    Define generation limit and offset values as constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,5 @@ const String appName = 'Pokédex';
 const String homeRouteName = 'Home';
 const String generationsRouteName = 'Generations';
 const String gamesRouteName = 'Games';
+const int minGen = 1;
+const int maxGen = 9;
