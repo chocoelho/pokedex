32e1eab907c494b72bd8c14b71d313dfc9ab67ca
Carlos Coelho <carlospecter@gmail.com>
2023-02-13 23:49:59 -0300

    Use constants for minGen and maxGen to list all generations

--- lib/services/generations.dart
+++ lib/services/generations.dart
@@ -1,10 +1,12 @@
 import 'package:pokeapi/model/game/generation.dart';
 import 'package:pokeapi/pokeapi.dart';
+import 'package:pokedex/constants.dart' as constants;
 
 Future<List<Generation>> fetchGenerationsList() {
-  return PokeAPI.getObjectList<Generation>(1, 9).then((generations) => generations
-      .map((generation) => Generation(id: generation?.id, name: generation?.name))
-      .toList());
+  return PokeAPI.getObjectList<Generation>(constants.minGen, constants.maxGen).then((generations) =>
+      generations
+          .map((generation) => Generation(id: generation?.id, name: generation?.name))
+          .toList());
 }
 
 Future<Generation> fetchGeneration() {
